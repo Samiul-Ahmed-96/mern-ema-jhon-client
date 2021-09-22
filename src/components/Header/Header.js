29b58524748954d9765e6eb96d454ee51e2d773d
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-22 21:24:20 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,15 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt="" />
+            <div className="nav">
+                <nav>
+                <ul>
+                    <li><a href="/shop">Shop</a></li>    
+                    <li><a href="/Order-review">Order Review</a></li>    
+                    <li><a href="/manage-inventory">Manage Inventory</a></li>    
+                </ul>
+                </nav>
+            </div>
         </div>
     );
 };
