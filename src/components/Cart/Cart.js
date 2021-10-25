af86dc7541cd34b46e4ab6ad9246abac065119a9
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-25 22:11:59 +0600

    Update project

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,24 +2,32 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
-    const {cart} = props;
-
-    let total = 0 ;
-    for(const product of cart){
-        total = total + product.price;
+    const { cart } = props;
+    // const totalReducer = (previous, product) => previous + product.price;
+    // const total = cart.reduce(totalReducer, 0);
+    let totalQuantity = 0;
+    let total = 0;
+    for (const product of cart) {
+        if (!product.quantity) {
+            product.quantity = 1;
+        }
+        total = total + product.price * product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
     }
-    const shipping = 0 < total ? 15 : 0;
-    const tax = (total+shipping) * .10;
-    const grandTotal = total+shipping+tax;
-    
+
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
     return (
-        <div className="cart-details">
-            <h2>Order Summary</h2>
-            <h3>Items Order : {cart.length}</h3>
-            <h3>Shipping : {shipping.toFixed(2)}</h3>
-            <h3>Tax : {tax.toFixed(2)}</h3>
-            <h3>Total: {total.toFixed(2)}</h3>
-            <h2>Grand Total : {grandTotal.toFixed(2)}</h2>
+        <div>
+            <h3>Order Summary</h3>
+            <h5>Items Ordered: {totalQuantity}</h5>
+            <br />
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+            {props.children}
         </div>
     );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,34 +1,34 @@
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import Rating from 'react-rating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
+import Rating from 'react-rating';
 
 const Product = (props) => {
-    const{name,seller,price,img,stock,star} = props.items;
-    const addToCart = props.handleAddToCart;
+    // console.log(props);
+    const { name, img, seller, price, stock, star } = props.product;
+
     return (
-        <div className="products-details">
+        <div className="product">
             <div>
                 <img src={img} alt="" />
             </div>
-            <div className="product-content">
-                <h3>{name}</h3>
-                <p><small>by : {seller}</small></p>
-                <h3>Price: {price}</h3>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <p><small>by: {seller}</small></p>
+                <p>Price: {price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
                 <Rating
-                className="rating"
-                initialRating = {star}
-                emptySymbol= "far fa-star"
-                fullSymbol = "fas fa-star"
-                readonly
-                >
-                </Rating>
-                <p><small> only {stock} left in stock - order soon</small></p>
-                <button  onClick={()=>addToCart(props.items)}>
-                <FontAwesomeIcon icon={faShoppingCart} />
-                <div>Add To Cart</div></button>
-            </div> 
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly></Rating>
+                <br />
+                <button
+                    onClick={() => props.handleAddToCart(props.product)}
+                    className="btn-regular"
+                ><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+            </div>
         </div>
     );
 };
