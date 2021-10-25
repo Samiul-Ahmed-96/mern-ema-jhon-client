c48eacf8dac79f1ecda2d642c59c34f9572a4cd3
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-26 00:04:37 +0600

    Load products from db

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,10 +1,9 @@
-import { useEffect } from "react";
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('./products.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, []);
